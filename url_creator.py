9a8fe0552eac4db70bc2a936884ed4ef5fdbe385
Shreeya Kannan <shreeyakannan@gmail.com>
2024-10-04 19:29:26 +0530

    url module created

--- url_creator.py
+++ url_creator.py
@@ -1,16 +1,32 @@
-from firebase_admin import credentials, initialize_app, storage
+from firebase_admin import credentials, initialize_app, storage, db
+from datetime import date
+import firebase_admin
 
-# Init firebase with your credentials
-cred = credentials.Certificate("credentials.json")
-initialize_app(cred, {'storageBucket': 'intel-oneapi.appspot.com'})
+def url(image_path):
+    cred = credentials.Certificate("credentials.json")
+    initialize_app(cred, {'storageBucket': 'intel-oneapi.appspot.com'})
 
-# Put your local file path 
-fileName = "satori.jpg"
-bucket = storage.bucket()
-blob = bucket.blob(fileName)
-blob.upload_from_filename(fileName)
+    # Put your local file path
+    fileName = "satori.jpg"
+    bucket = storage.bucket()
+    blob = bucket.blob(fileName)
+    blob.upload_from_filename(fileName)
 
-# Opt : if you want to make public access from the URL
-blob.make_public()
+    # Opt : if you want to make public access from the URL
+    result = blob.make_public()
+    return result
 
-print("your file url", blob.public_url)
+def add_history(uid,png):
+    current_date = date.today()
+    formatted_date = current_date.strftime("%B %d, %Y")
+    img_url = url(png)
+
+    # Initialize Firebase app
+    cred = credentials.Certificate("credentials.json")
+    firebase_admin.initialize_app(cred, {
+        'databaseURL': 'https://intel-oneapi-default-rtdb.asia-southeast1.firebasedatabase.app/'
+    })
+
+    user_ref = db.reference(f'/{uid}/history/{formatted_date}')
+    user_ref.set({'ecgImg': img_url})
+    print("updation successful")
